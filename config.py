c894edda162f6ca2d785e498e075257a774d20b8
Saidazim0323 <150612742+Saidazim0323@users.noreply.github.com>
2025-12-05 18:32:34 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
 DATABASE_URL = os.getenv("DATABASE_URL")
-SUPER_ADMIN_ID = int(os.getenv("SUPER_ADMIN_ID"))
+
+SUPER_ADMIN_ID = os.getenv("SUPER_ADMIN_ID")
+if SUPER_ADMIN_ID is not None:
+    SUPER_ADMIN_ID = int(SUPER_ADMIN_ID)
+
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
