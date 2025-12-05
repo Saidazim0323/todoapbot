9898094a2cd20ec33a43e322c7a56413dd850967
Saidazim0323 <150612742+Saidazim0323@users.noreply.github.com>
2025-12-05 19:11:23 +0500

    Update task_handlers.py

--- handlers/task_handlers.py
+++ handlers/task_handlers.py
@@ -1,21 +1,20 @@
-from aiogram import Router, types
+from aiogram import Router, types, F
 from database import create_pool
 
 task_router = Router()
 
-@task_router.message(commands=["task"])
+@task_router.message(F.text.startswith("/task"))
 async def add_task(msg: types.Message):
     title = msg.text.replace("/task", "").strip()
     if not title:
         return await msg.answer("Vazifa yozing: /task Kitob o‘qish")
 
+    # DB dan pool olish
     pool = await create_pool()
-
     async with pool.acquire() as conn:
         await conn.execute(
             "INSERT INTO tasks (user_id, title) VALUES ($1, $2)",
-            msg.from_user.id,
-            title
+            msg.from_user.id, title
         )
 
     await msg.answer("✅ Vazifa qo‘shildi.")
