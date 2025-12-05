dc8afb67366d04c735eb979b0976ee744318e807
Saidazim0323 <150612742+Saidazim0323@users.noreply.github.com>
2025-12-05 19:18:45 +0500

    Update task_handlers.py

--- handlers/task_handlers.py
+++ handlers/task_handlers.py
@@ -6,10 +6,10 @@ task_router = Router()
 @task_router.message(F.text.startswith("/task"))
 async def add_task(msg: types.Message):
     title = msg.text.replace("/task", "").strip()
+
     if not title:
         return await msg.answer("Vazifa yozing: /task Kitob o‘qish")
 
-    # DB dan pool olish
     pool = await create_pool()
     async with pool.acquire() as conn:
         await conn.execute(
