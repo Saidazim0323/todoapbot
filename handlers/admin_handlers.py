cd42b82e3ddcd08eb386c6097512559b05d554f7
Saidazim0323 <150612742+Saidazim0323@users.noreply.github.com>
2025-12-05 20:58:46 +0500

    Update admin_handlers.py

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -1,6 +1,6 @@
-# handlers/admin_handlers.py
 from aiogram import Router
 from aiogram.types import Message
+from aiogram.filters import Command
 from config import SUPER_ADMIN_ID
 
 admin_router = Router()
@@ -8,10 +8,9 @@ admin_router = Router()
 def register_admin_handlers(dp):
     dp.include_router(admin_router)
 
-
-@admin_router.message()
+@admin_router.message(Command("admin"))
 async def admin_panel(msg: Message):
     if msg.from_user.id != SUPER_ADMIN_ID:
-        return
-    
+        return await msg.answer("❌ Siz adminsiz!")
+
     await msg.answer("🔐 Admin panelga xush kelibsiz!")
