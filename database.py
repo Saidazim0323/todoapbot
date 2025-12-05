d96c4c859802f197aa0642edd75820ba9bc49018
Saidazim0323 <150612742+Saidazim0323@users.noreply.github.com>
2025-12-05 17:55:29 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,36 +1,39 @@
 import asyncpg
-import os
 from config import DATABASE_URL
 
-async def connect_db():
-    return await asyncpg.connect(DATABASE_URL)
+db_pool = None
+
+async def create_pool():
+    global db_pool
+    if db_pool is None:
+        db_pool = await asyncpg.create_pool(DATABASE_URL)
+    return db_pool
+
 
 async def create_tables():
-    conn = await connect_db()
+    pool = await create_pool()
+    async with pool.acquire() as conn:
+        await conn.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                user_id BIGINT UNIQUE,
+                reminder_hour INT DEFAULT 9
+            );
+        """)
 
-    await conn.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            user_id BIGINT UNIQUE,
-            reminder_hour INT DEFAULT 9
-        );
-    """)
+        await conn.execute("""
+            CREATE TABLE IF NOT EXISTS tasks (
+                id SERIAL PRIMARY KEY,
+                user_id BIGINT,
+                title TEXT,
+                is_done BOOLEAN DEFAULT FALSE
+            );
+        """)
 
-    await conn.execute("""
-        CREATE TABLE IF NOT EXISTS tasks (
-            id SERIAL PRIMARY KEY,
-            user_id BIGINT,
-            title TEXT,
-            is_done BOOLEAN DEFAULT FALSE
-        );
-    """)
-
-    await conn.execute("""
-        CREATE TABLE IF NOT EXISTS habits (
-            id SERIAL PRIMARY KEY,
-            user_id BIGINT,
-            habit TEXT
-        );
-    """)
-
-    await conn.close()
+        await conn.execute("""
+            CREATE TABLE IF NOT EXISTS habits (
+                id SERIAL PRIMARY KEY,
+                user_id BIGINT,
+                habit TEXT
+            );
+        """)
