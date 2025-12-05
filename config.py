abd8e1254b1dffcf1664ff3c25b231c7b8be73a0
Saidazim0323 <150612742+Saidazim0323@users.noreply.github.com>
2025-12-05 17:39:44 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # https://your-app.onrender.com/
-WEBAPP_HOST = "0.0.0.0"
-WEBAPP_PORT = int(os.getenv("PORT", 8000))
-
-DEFAULT_REMINDER_HOUR = int(os.getenv("DEFAULT_REMINDER_HOUR", 9))
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
-DATABASE_URL = os.getenv("DATABASE_URL")  # Postgres URL
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
+SUPER_ADMIN_ID = int(os.getenv("SUPER_ADMIN_ID"))
