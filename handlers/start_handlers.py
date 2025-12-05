a0a624818aa70f11164a1ea6155c015b9fa44523
Saidazim0323 <150612742+Saidazim0323@users.noreply.github.com>
2025-12-05 21:31:26 +0500

    Update start_handlers.py

--- handlers/start_handlers.py
+++ handlers/start_handlers.py
@@ -1,13 +1,19 @@
-# handlers/start_handlers.py
 from aiogram import Router, types
-from aiogram.filters import CommandStart
+from aiogram.filters import Command
 
 start_router = Router()
 
-@start_router.message(CommandStart())
-async def start_cmd(msg: types.Message):
-    await msg.answer("👋 Assalomu alaykum! Men ishlayapman.")
-    
-
 def register_start_handlers(dp):
     dp.include_router(start_router)
+
+@start_router.message(Command("start"))
+async def start_cmd(msg: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=[
+            [types.KeyboardButton(text="/task"), types.KeyboardButton(text="/habit")],
+            [types.KeyboardButton(text="/admin"), types.KeyboardButton(text="/time")]
+        ],
+        resize_keyboard=True
+    )
+
+    await msg.answer("📌 Menyudan birini tanlang:", reply_markup=keyboard)
