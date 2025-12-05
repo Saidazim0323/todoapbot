cee8f88c219e489b4a0097fee413e8fd9a1bc763
Saidazim0323 <150612742+Saidazim0323@users.noreply.github.com>
2025-12-05 21:14:55 +0500

    Update admin_handlers.py

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -1,3 +1,4 @@
+# handlers/admin_handlers.py
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
@@ -7,10 +8,14 @@ admin_router = Router()
 
 def register_admin_handlers(dp):
     dp.include_router(admin_router)
+@admin_router.message(Command("start"))
+async def start_handler(msg: Message):
+    await msg.answer("Assalomu alaykum! Botdan foydalanishni boshlashingiz mumkin 😊")
 
+# Faqat /admin komandasi kelganda ishlaydi
 @admin_router.message(Command("admin"))
 async def admin_panel(msg: Message):
     if msg.from_user.id != SUPER_ADMIN_ID:
-        return await msg.answer("❌ Siz adminsiz!")
+        return await msg.answer("❌ Siz admin emassiz!")
 
-    await msg.answer("🔐 Admin panelga xush kelibsiz!")
+    await msg.answer("🔐 Admin paneliga xush kelibsiz!")
