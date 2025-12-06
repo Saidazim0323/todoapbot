0e5ecb4b8d1558fd8682f297cda4a74140452f32
Saidazim0323 <150612742+Saidazim0323@users.noreply.github.com>
2025-12-06 07:49:19 +0500

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -4,20 +4,16 @@ from database import create_pool
 
 async def send_reminders(bot):
     pool = await create_pool()
-
     async with pool.acquire() as conn:
-        rows = await conn.fetch(
-            "SELECT user_id, text FROM reminders WHERE remind_at <= NOW()"
-        )
+        rows = await conn.fetch("SELECT user_id, text FROM reminders WHERE remind_at <= NOW()")
 
     for row in rows:
         try:
             await bot.send_message(row["user_id"], f"🔔 Eslatma: {row['text']}")
-        except Exception as e:
-            print("Xato:", e)
-
+        except:
+            pass
 
 async def start_scheduler(bot):
     while True:
         await send_reminders(bot)
-        await asyncio.sleep(60)   # har 1 daqiqada tekshiradi
+        await asyncio.sleep(60)
